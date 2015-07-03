3e79dc85444b3942e7885ffb8886c49186fa59f9
gKonghp <gkonghp@outlook.com>
2015-07-03 14:32:54 +0800

    OK

---
+++ PTMSAssignment/src/com/assignment/ptmsassignment/MainActivity.java
@@ -0,0 +1,41 @@
+package com.assignment.ptmsassignment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	Button btnLogout;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findView();
+        
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, LoginSystem.class);
+				startActivity(intent);
+				finish();
+			}
+		});
+    }
+
+    public void findView(){
+    	btnLogout = (Button)findViewById(R.id.btnLogout);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
